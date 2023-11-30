2f2f850982d131ceb8ee9ec0dcb83e85cc6fe2c1
Dong Jin <cdj7235@naver.com>
2023-12-01 02:17:48 +0900

    아이디 중복검사 코드 수정

--- user/check.php
+++ user/check.php
@@ -1,25 +1,27 @@
 <?php
-	   $host = 'localhost:3306';
-	   $user = 'root';
-	   $pw = '1234';
-	   $db_name = 'dbproject';
-	   $mysqli = new mysqli($host, $user, $pw, $db_name); //db 연결
+$host = 'localhost:3306';
+$user = 'root';
+$pw = '1234';
+$db_name = 'dbproject';
+$mysqli = new mysqli($host, $user, $pw, $db_name); // db 연결
 
-	$uid = $_GET["userid"];
+$userid = $_GET["userid"];
 
-	$sql = mq("select * from member where id='".$uid."'");
-	$member = $sql->fetch_array();
+// SQL 쿼리를 문자열로 작성할 때는 큰따옴표 안에서 작은따옴표로 묶어줍니다.
+$sql = "SELECT * FROM user WHERE userid='".$userid."'";
+$result = $mysqli->query($sql);
 
-	if($member==0)
-	{
-?>
-	<div style='font-family:"malgun gothic"';><?php echo $uid; ?>는 사용가능한 아이디입니다.</div>
-<?php 
-	}else{
-?>
-	<div style='font-family:"malgun gothic"; color:red;'><?php echo $uid; ?>중복된아이디입니다.<div>
-<?php
-	}
+// fetch_array()는 결과가 없을 때 false를 반환합니다.
+if($result->num_rows == 0) {
+    ?>
+    <div style='font-family:"malgun gothic";'><?php echo $userid; ?>는 사용가능한 아이디입니다.</div>
+    <?php 
+} else {
+    $member = $result->fetch_array();
+    ?>
+    <div style='font-family:"malgun gothic"; color:red;'><?php echo $userid; ?> 중복된 아이디입니다.</div>
+    <?php
+}
 ?>
 <button value="닫기" onclick="window.close()">닫기</button>
 <script>
