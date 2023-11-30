cd5c05128064412f890d062f7b26ccba5d723c8f
Dong Jin <cdj7235@naver.com>
2023-11-30 21:17:04 +0900

    add check.php

---
+++ user/check.php
@@ -0,0 +1,26 @@
+<?php
+	   $host = 'localhost:3306';
+	   $user = 'root';
+	   $pw = '1234';
+	   $db_name = 'dbproject';
+	   $mysqli = new mysqli($host, $user, $pw, $db_name); //db 연결
+
+	$uid = $_GET["userid"];
+
+	$sql = mq("select * from member where id='".$uid."'");
+	$member = $sql->fetch_array();
+
+	if($member==0)
+	{
+?>
+	<div style='font-family:"malgun gothic"';><?php echo $uid; ?>는 사용가능한 아이디입니다.</div>
+<?php 
+	}else{
+?>
+	<div style='font-family:"malgun gothic"; color:red;'><?php echo $uid; ?>중복된아이디입니다.<div>
+<?php
+	}
+?>
+<button value="닫기" onclick="window.close()">닫기</button>
+<script>
+</script>
