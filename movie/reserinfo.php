4bd5c532457240d71b1d5c50ff0807015e3a90e9
Dong Jin <cdj7235@naver.com>
2023-12-03 03:16:18 +0900

    reserinfo.php 수정

--- movie/reserinfo.php
+++ movie/reserinfo.php
@@ -1,16 +1,47 @@
 <?php
-
 session_start();
 $host = 'localhost:3306';
 $user = 'root';
 $pw = '1234';
 $db_name = 'dbproject';
-$mysqli = new mysqli($host, $user, $pw, $db_name); //db 연결
+$conn = new mysqli($host, $user, $pw, $db_name); //db 연결
 
-if (!isset($_SESSION['userid'])) {
-    header('Location: ../index.php');
-    exit();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-echo 1;
+// Fetch data from paylist table
+$paylist_data_query = "SELECT * FROM paylist";
+$paylist_data_result = $conn->query($paylist_data_query);
+
+if ($paylist_data_result->num_rows > 0) {
+    echo "Data in paylist table:<br>";
+    while ($row = $paylist_data_result->fetch_assoc()) {
+        foreach ($row as $key => $value) {
+            echo "$key: $value<br>";
+        }
+        echo "<br>";
+    }
+} else {
+    echo "No data found in paylist table";
+}
+
+// Fetch data from pay table
+$pay_data_query = "SELECT * FROM pay";
+$pay_data_result = $conn->query($pay_data_query);
+
+if ($pay_data_result->num_rows > 0) {
+    echo "<br>Data in pay table:<br>";
+    while ($row = $pay_data_result->fetch_assoc()) {
+        foreach ($row as $key => $value) {
+            echo "$key: $value<br>";
+        }
+        echo "<br>";
+    }
+} else {
+    echo "No data found in pay table";
+}
+
+// Close the connection
+$conn->close();
 ?>
