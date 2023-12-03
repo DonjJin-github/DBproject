83d853cf69a79826c975e5eeffac38370baf5c12
Dong Jin <cdj7235@naver.com>
2023-12-03 22:00:52 +0900

    reserinfo.php 쿼리문 수정

--- movie/reserinfo.php
+++ movie/reserinfo.php
@@ -4,55 +4,98 @@ $host = 'localhost:3306';
 $user = 'root';
 $pw = '1234';
 $db_name = 'dbproject';
-$conn = new mysqli($host, $user, $pw, $db_name); //db 연결
+$mysqli = new mysqli($host, $user, $pw, $db_name); //db 연결
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// 로그인 여부 확인
+if (!isset($_SESSION['userid'])) {
+    header('Location: ../index.php');
+    exit();
 }
 
-// Get the current username from the session
-$currentUsername = $_SESSION['userid'];
+// userid를 세션에서 가져오기
+$userid = $_SESSION['userid'];
 
-// Query to fetch user_id based on the current username
-$userQuery = "SELECT Uid FROM user WHERE userid = '$currentUsername'";
-$userResult = $conn->query($userQuery);
+$sql = $mysqli->prepare("SELECT * FROM user WHERE userid = ?");
+$sql->bind_param("s", $userid);
+$sql->execute();
+$result = $sql->get_result(); // 결과 가져오기
+$row = $result->fetch_assoc();
+?>
 
-if ($userResult->num_rows > 0) {
-    $userRow = $userResult->fetch_assoc();
-    $currentUserId = $userRow['Uid'];
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>마이페이지</title>
+    <link href="../default.css" rel="stylesheet" />
+</head>
+<body>
+<div id="full">
+        <div id="banner">
+        <div id="main"><a href='../main.php'><p>메인 화면</p></div>
+            <div id="infor"><a href=''><p>예매 확인</p></div>
+            <div id="reser"><a href='../movie/process.php'>영화 예매</a></div>
+            <div id="mypage"><a href='../user/mypage.php'>마이 페이지</a></div>         
+			<?php
+			if(isset($_SESSION['userid'])){
+				echo "<div id='login'>{$_SESSION['userid']} 님 환영합니다.<a href='/user/logout.php'><input type='button' value='로그아웃' /></a></div>";
+			?>
+			<?php 
+				}else{
+				echo "<div id='login'><a href='index.php'>로그인</a></div>";
+			} 
+			?>
+        </div>
+        <div id="content" style="font-size : 20px">
+            <?php
+            $currentUsername = $_SESSION['userid'];
 
-    // Query to fetch payUid, seat_id, seatname, movie_id, moviename, and movietime based on the current user's ID
-    $sql = "SELECT pay.payUid, paylist.seat_id, seat.seatname, seat.movie_id, movie.moviename, movie.movietime
-            FROM pay
-            JOIN paylist ON pay.payUid = paylist.pay_id
-            JOIN seat ON paylist.seat_id = seat.seatUid
-            JOIN movie ON seat.movie_id = movie.movieUid
-            WHERE pay.user_id = $currentUserId";
+            // Query to fetch user_id based on the current username
+            $userQuery = "SELECT Uid FROM user WHERE userid = '$currentUsername'";
+            $userResult = $mysqli->query($userQuery);
 
-    $result = $conn->query($sql);
+            if ($userResult->num_rows > 0) {
+                $userRow = $userResult->fetch_assoc();
+                $currentUserId = $userRow['Uid'];
 
-    if ($result->num_rows > 0) {
-        $prevPayUid = null; // Variable to store the previous payUid
+                // Query to fetch payUid, seat_id, seatname, movie_id, moviename, and movietime based on the current user's ID
+                $sql = "SELECT pay.payUid, paylist.seat_id, seat.seatname, seat.movie_id, movie.moviename, movie.movietime
+                        FROM pay
+                        JOIN paylist ON pay.payUid = paylist.pay_id
+                        JOIN seat ON paylist.seat_id = seat.seatUid
+                        JOIN movie ON seat.movie_id = movie.movieUid
+                        WHERE pay.user_id = $currentUserId";
 
-        // Iterate over the remaining rows
-        while ($row = $result->fetch_assoc()) {
-            if ($prevPayUid !== $row["payUid"]) {
-                echo "<br>";
-                echo "예약번호" . $row["payUid"] . "<br>";
-                echo " 영화제목 : " . $row["moviename"]. "<br>";
-                echo " 상영시간 : " . $row["movietime"]. "<br>";
-                echo " 좌석번호 : " ;
-                $prevPayUid = $row["payUid"];
+                $result = $mysqli->query($sql);
+
+                if ($result->num_rows > 0) {
+                    $prevPayUid = null; // Variable to store the previous payUid
+
+                    // Iterate over the remaining rows
+                    while ($row = $result->fetch_assoc()) {
+                        if ($prevPayUid !== $row["payUid"]) {
+                            echo "<br>";
+                            echo " 예매번호 : " . $row["payUid"] . "<br>";
+                            echo " 영화제목 : " . $row["moviename"]. "<br>";
+                            echo " 상영시간 : " . $row["movietime"]. "<br>";
+                            echo " 좌석번호 : " ;
+                            $prevPayUid = $row["payUid"];
+                        }
+                        echo $row["seatname"] . " ";
+                    }
+                } else {
+                    echo "예약 정보 없음";
+                }
+            } else {
+                echo "User not found";
             }
 
-            echo $row["seatname"] . " ";
-        }
-    } else {
-        echo "예약 정보 없음";
-    }
-} else {
-    echo "User not found";
-}
+            $mysqli->close();
+            ?>
 
-$conn->close();
-?>
+        </div>
+    </div>
+</body>
+</html>
+
+
